ab3aaa96edb99a4bac30aa441ff8c7cad63be9b7
gmarcos9 <gmarcos9@corp.vodafone.es>
2023-06-14 20:24:13 +0200

    Initial Commit

---
+++ GonzaloMRZaraRyM/GonzaloMRZaraRyMApp.swift
@@ -0,0 +1,17 @@
+//
+//  GonzaloMRZaraRyMApp.swift
+//  GonzaloMRZaraRyM
+//
+//  Created by neoris on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GonzaloMRZaraRyMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
